d022be6db24e2cad189bec3601f9721dded7eae7
andru-oca <112781394+andru-oca@users.noreply.github.com>
2024-04-15 20:15:05 -0300

    js

---
+++ 13_clase/javascript-clase/assets/js/index.js
@@ -0,0 +1,33 @@
+// alert("Hola! Usuario # 1");
+// console.log("hola, mundo desde la terminal");
+// let nombre = prompt("Hola ... ingresa tu nombre");
+
+// document.write(
+// `
+// <marquee >
+//     <h1 style="color: rebeccapurple;">Bienvenido ` + nombre + ` </h1>
+// </marquee>
+// `) ;
+
+// // console.log("El nombre del usuario es: >> " + nombre );
+
+
+// console.log("Hola " + " mundo");
+
+
+
+let edad = prompt("Hola, decime tu edad");
+
+let randomNumber = (Number(edad) + 10 )  * 1.25 - 5 ;
+
+document.write(
+`
+<div class="color">
+    <p>
+        El numero random es: `+ randomNumber+`
+    </p>
+</div>
+`
+)
+
+
