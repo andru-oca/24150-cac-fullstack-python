59cc9d2356a1f9a541fbf175b83f474ff2c2a0dd
anderson <anderson.ocana@bue.edu.ar>
2024-05-08 20:09:00 -0300

    clase 20

--- 19_clase/assets/js/index.js
+++ 19_clase/assets/js/index.js
@@ -42,30 +42,36 @@ class Comments {
 //     ul.innerHTML = texto;
 //   });
 
-
-
 // MODULO OBJETO
 class Viaje {
-  constructor({ nombre_ciudad,precio,oferta,descuento,pasajes }) {
+  constructor({ nombre_ciudad, precio, oferta, descuento, pasajes }) {
     this.nombreCiudad = nombre_ciudad;
-    this.precio= precio;
+    this.precio = precio;
     this.oferta = oferta;
     this.descuento = descuento;
-    this.pasajes = pasajes
+    this.pasajes = pasajes;
+    this.precioNumerico = this.transformPrecio()
   }
 
+  transformPrecio(){
+    let amountWithoutSymbols = this.precio.replace(/[$ ,]/g, "");
+    return parseFloat(amountWithoutSymbols);
+
+  }
 
   printDisplay() {
     let container = document.getElementById("container");
 
     let div = document.createElement("div");
 
+    div.classList.add("viaje");
     div.innerHTML = `
+            <button class="closer"> x </button>
             <h3 class="color">${this.nombreCiudad}</h3>
-            <ul>
-                <li>${this.precio}</li>
-                <li>${this.descuento}</li>
-            </ul>
+            
+                <p>${this.precio}</p>
+                <p>${this.descuento}</p>
+            
             <details>
                 <summary> Detalles extras</summary>
                 ${this.pasajes} | ${this.oferta}
@@ -78,12 +84,35 @@ class Viaje {
 
 // El endpoint donde buscar la dar
 const URL = "https://sheetdb.io/api/v1/tfiz44jksqe8l";
-
+let password = "zag75vv1p77717zk3pbly23snparz1lexe0z778d";
 // la accion de hacer un consumo desde esa api
 
-fetch(URL)
-.then(res => res.json())
-.then(data => data.map(d => new Viaje(d)).forEach(viaje => viaje.printDisplay()))
-.catch(error => console.log({error}))
+const headers = { Authorization: `Bearer ${password}` };
 
+const loadingData = () =>
+{  
+  // consume data desde la api
+  fetch(URL,{headers})
+    // transforma ese json en un array
+    .then((res) => res.json())
+    // transforma cada elemento en un objeto viaje
+    .then((data) =>
+      data.map((d) => new Viaje(d))
+    )
+    // filtra los objetos que tienen un valor menor al que dice en el input
+    .then(data => {
+      let valor = document.querySelector("#filtroPrecio").valueAsNumber;
+      return data.filter(viaje => viaje.precioNumerico < valor )
+    })
+    // inyecta dentro de la pantalla con ese filtro
+    .then(data => data.forEach(viaje => viaje.printDisplay()))
+    .catch((error) => console.log({ error }));
+}
+const clearData = () => {
+  let container = document.getElementById("container");
+  container.innerHTML = "";
+};
+
+let btn = document.querySelector("#loadData");
+btn.addEventListener("click", loadingData);
 
