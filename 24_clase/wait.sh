3e182a9b1bc409b9d45ad1ffc0cca8e6c01244d1
andru-oca <112781394+andru-oca@users.noreply.github.com>
2024-05-20 20:33:00 +0000

    updates

--- 24_clase/wait.sh
+++ 24_clase/wait.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-until docker compose logs mysql-server | grep "/usr/sbin/mysqld: ready for connections."; do 
+until docker compose logs mysql | grep "/usr/sbin/mysqld: ready for connections."; do 
     echo -n .; 
     sleep 1;
 done
 
-sleep 10
+sleep 5
